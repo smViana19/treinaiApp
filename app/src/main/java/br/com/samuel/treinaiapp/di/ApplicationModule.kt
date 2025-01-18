1c7e6219e8c99e4a46435da57b154881c123f42b
Samuu <samuelfilipevianadois@gmail.com>
2025-01-17 23:17:38 -0300

    feat: configure app provides

---
+++ app/src/main/java/br/com/samuel/treinaiapp/di/ApplicationModule.kt
@@ -0,0 +1,46 @@
+package br.com.samuel.treinaiapp.di
+
+import br.com.samuel.treinaiapp.data.remote.api.ApiExerciseCategoryService
+import br.com.samuel.treinaiapp.data.remote.api.ApiExerciseService
+import br.com.samuel.treinaiapp.utils.API_WGER
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+  @Provides
+  fun provideOkHttpClient() : OkHttpClient {
+    return OkHttpClient.Builder()
+      .build()
+  }
+
+  @Provides
+  fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder()
+      .baseUrl(API_WGER)
+      .client(okHttpClient)
+      .addConverterFactory(GsonConverterFactory.create())
+      .build()
+  }
+
+  @Provides
+  @Singleton
+  fun provideExerciseCategoryService(retrofit: Retrofit): ApiExerciseCategoryService {
+    return retrofit.create(ApiExerciseCategoryService::class.java)
+  }
+
+  @Provides
+  @Singleton
+
+  fun provideExerciseService(retrofit: Retrofit): ApiExerciseService {
+    return retrofit.create(ApiExerciseService::class.java)
+  }
+}
