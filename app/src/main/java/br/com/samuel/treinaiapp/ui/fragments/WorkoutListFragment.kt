e3f73695fcd5a692ad3fe44c5728b43bb040fa1c
Samuu <samuelfilipevianadois@gmail.com>
2025-01-20 15:15:12 -0300

    refactor: removed unused activities

--- app/src/main/java/br/com/samuel/treinaiapp/ui/fragments/WorkoutListFragment.kt
+++ app/src/main/java/br/com/samuel/treinaiapp/ui/fragments/WorkoutListFragment.kt
@@ -1,60 +1,45 @@
 package br.com.samuel.treinaiapp.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.samuel.treinaiapp.R
+import br.com.samuel.treinaiapp.databinding.FragmentWorkoutListBinding
+import br.com.samuel.treinaiapp.ui.adapters.WorkoutAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WorkoutListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WorkoutListFragment : Fragment() {
-  // TODO: Rename and change types of parameters
-  private var param1: String? = null
-  private var param2: String? = null
-
+  private lateinit var binding: FragmentWorkoutListBinding
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    arguments?.let {
-      param1 = it.getString(ARG_PARAM1)
-      param2 = it.getString(ARG_PARAM2)
-    }
+
   }
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_workout_list, container, false)
   }
 
-  companion object {
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WorkoutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    @JvmStatic
-    fun newInstance(param1: String, param2: String) =
-      WorkoutListFragment().apply {
-        arguments = Bundle().apply {
-          putString(ARG_PARAM1, param1)
-          putString(ARG_PARAM2, param2)
-        }
-      }
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding = FragmentWorkoutListBinding.bind(view)
+
+    binding.imageButtonBack.setOnClickListener {
+      findNavController().popBackStack()
+    }
+
+
+    binding.recyclerViewListWorkouts.layoutManager = LinearLayoutManager(context)
+    val items = listOf("Treino A", "Treino B", "Treino C", "Treino D")
+    val adapter = WorkoutAdapter(items)
+    binding.recyclerViewListWorkouts.adapter = adapter
+
+
+
   }
 }
