2daca419bb1c6e6e807c6e6778b04b7ec049652a
Samuu <samuelfilipevianadois@gmail.com>
2025-01-19 22:33:18 -0300

    feat: added fragments - modified and retrieve activities changed to fragments - added bottom navigation - improved layout home screen - added new screens -> NotificationFragment.kt, fragment_home.xml, fragment_notification.xml

--- app/src/main/java/br/com/samuel/treinaiapp/data/remote/api/ApiExerciseService.kt
+++ app/src/main/java/br/com/samuel/treinaiapp/data/remote/api/ApiExerciseService.kt
@@ -7,6 +7,7 @@ import retrofit2.http.Query
 interface ApiExerciseService {
   @GET("exercise")
   suspend fun getExercises(
+    @Query("language") language: Int = 7,
     @Query("limit") limit: Int = 20,
     @Query("offset") offset: Int = 0
   ): ApiExerciseResponse
