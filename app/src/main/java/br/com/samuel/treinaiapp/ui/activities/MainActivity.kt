033347ef8b104486e918ac1a48de2c99a54e9484
Samuu <samuelfilipevianadois@gmail.com>
2025-01-19 22:31:29 -0300

    feat: added jetpack navigation graph

--- app/src/main/java/br/com/samuel/treinaiapp/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/samuel/treinaiapp/ui/activities/MainActivity.kt
@@ -2,21 +2,31 @@ package br.com.samuel.treinaiapp.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.samuel.treinaiapp.R
+import br.com.samuel.treinaiapp.databinding.ActivityMainBinding
 import br.com.samuel.treinaiapp.ui.adapters.WorkoutAdapter
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    val intent = Intent(this, WorkoutActivity::class.java)
-    startActivity(intent)
-    finish()
+//    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    val navController = navHostFragment.navController
   }
 }
