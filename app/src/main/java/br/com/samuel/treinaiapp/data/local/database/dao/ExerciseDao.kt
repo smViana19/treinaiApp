21bf7042ed0149e6d7a7196b5894b18b23c36d08
Samuu <samuelfilipevianadois@gmail.com>
2025-01-21 20:38:47 -0300

    bla

--- app/src/main/java/br/com/samuel/treinaiapp/data/local/database/dao/ExerciseDao.kt
+++ app/src/main/java/br/com/samuel/treinaiapp/data/local/database/dao/ExerciseDao.kt
@@ -1,6 +1,5 @@
 package br.com.samuel.treinaiapp.data.local.database.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
