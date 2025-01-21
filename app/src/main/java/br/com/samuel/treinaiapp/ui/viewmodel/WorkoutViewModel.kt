57c6b84b01e6e779baccda5dfe294e6ac670ded1
Samuu <samuelfilipevianadois@gmail.com>
2025-01-21 20:36:02 -0300

    feat: created and configure workout viewmodel

--- app/src/main/java/br/com/samuel/treinaiapp/ui/viewmodel/WorkoutViewModel.kt
+++ app/src/main/java/br/com/samuel/treinaiapp/ui/viewmodel/WorkoutViewModel.kt
@@ -1,4 +1,41 @@
 package br.com.samuel.treinaiapp.ui.viewmodel
 
-class WorkoutViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.samuel.treinaiapp.data.local.database.model.WorkoutModel
+import br.com.samuel.treinaiapp.data.repository.WorkoutRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+class WorkoutViewModel @Inject constructor(
+  private val workoutRepository: WorkoutRepository
+) : ViewModel() {
+  private val _workouts = MutableLiveData<List<WorkoutModel>>()
+  val workouts: LiveData<List<WorkoutModel>> = _workouts
+
+  fun getWorkouts() {
+    viewModelScope.launch {
+      val workoutsData = workoutRepository.getWorkoutsFromLocalDatabase()
+      _workouts.postValue(workoutsData)
+//      _workouts.value = workoutsData
+//      println("workouts: $workoutsData")
+    }
+  }
+
+  fun insertWorkoutsToLocalDatabase(name: String, description: String?) {
+    viewModelScope.launch {
+      try {
+        workoutRepository.insertWorkoutsToLocalDatabase(name, description)
+        getWorkouts()
+        println("CHEGOU AQ")
+      } catch (e: Exception) {
+        e.printStackTrace()
+      }
+    }
+
+  }
+
 }
