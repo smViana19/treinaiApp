a818bd3289a1f782d2519e66540d2fc8a3933428
Samuu <samuelfilipevianadois@gmail.com>
2025-01-29 21:14:59 -0300

    feat: add new provides di

--- app/src/main/java/br/com/samuel/treinaiapp/di/ApplicationModule.kt
+++ app/src/main/java/br/com/samuel/treinaiapp/di/ApplicationModule.kt
@@ -2,8 +2,10 @@ package br.com.samuel.treinaiapp.di
 
 import br.com.samuel.treinaiapp.data.local.converter.Converters
 import br.com.samuel.treinaiapp.data.local.database.dao.ExerciseDao
+import br.com.samuel.treinaiapp.data.local.database.dao.ExerciseLogsDao
 import br.com.samuel.treinaiapp.data.local.database.dao.WorkoutDao
 import br.com.samuel.treinaiapp.data.remote.api.ApiExerciseService
+import br.com.samuel.treinaiapp.data.repository.ExerciseLogsRepository
 import br.com.samuel.treinaiapp.data.repository.ExerciseRepository
 import br.com.samuel.treinaiapp.data.repository.WorkoutRepository
 import dagger.Module
@@ -37,5 +39,12 @@ object ApplicationModule {
     return ExerciseRepository(apiExerciseService, exerciseDao, converters)
   }
 
+  @Provides
+  @Singleton
+  fun provideExerciseLogsRepository(
+    exerciseLogsDao: ExerciseLogsDao
+  ): ExerciseLogsRepository {
+    return ExerciseLogsRepository(exerciseLogsDao)
+  }
 
 }
--- app/src/main/java/br/com/samuel/treinaiapp/di/DatabaseModule.kt
+++ app/src/main/java/br/com/samuel/treinaiapp/di/DatabaseModule.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.room.Room
 import br.com.samuel.treinaiapp.data.local.database.AppDatabase
 import br.com.samuel.treinaiapp.data.local.database.dao.ExerciseDao
+import br.com.samuel.treinaiapp.data.local.database.dao.ExerciseLogsDao
 import br.com.samuel.treinaiapp.data.local.database.dao.WorkoutDao
 import dagger.Module
 import dagger.Provides
@@ -30,6 +31,11 @@ object DatabaseModule {
   @Provides
   fun provideWorkoutDao(appDatabase: AppDatabase): WorkoutDao {
     return appDatabase.getWorkoutDao()
+  }
+
+  @Provides
+  fun provideExerciseLogsDao(appDatabase: AppDatabase): ExerciseLogsDao {
+    return appDatabase.getExerciseLogsDao()
   }
 
 
