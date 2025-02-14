5adbc322793a4019fad3fcc4e5df7f37f6a692aa
Samuu <samuelfilipevianadois@gmail.com>
2025-02-14 15:53:06 -0300

    feat: add argumento to navigate to screen in flow

--- app/src/main/java/br/com/samuel/treinaiapp/ui/adapters/WorkoutDetailsAdapter.kt
+++ app/src/main/java/br/com/samuel/treinaiapp/ui/adapters/WorkoutDetailsAdapter.kt
@@ -1,8 +1,11 @@
 package br.com.samuel.treinaiapp.ui.adapters
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import br.com.samuel.treinaiapp.R
 import br.com.samuel.treinaiapp.data.local.database.model.ExerciseModel
 import br.com.samuel.treinaiapp.databinding.CardItemBinding
 
@@ -20,14 +23,23 @@ class WorkoutDetailsAdapter(
     val exercise = exercises[position]
     holder.binding.textExerciseName.text = exercise.name
     holder.binding.textDescription.text = exercise.description
-    //TODO ADICIONAR A EDIÇÃO DO CARD
-
+    holder.binding.cardViewListExercises.setOnClickListener {
+      val navController = it.findNavController()
+      val bundle = Bundle().apply {
+        putInt("exerciseId", exercise.id)
+      }
+      navController.navigate(
+        resId = R.id.action_workoutDetailsFragment_to_exerciseDetailsFragment,
+        args = bundle
+      )
+    }
   }
+
   override fun getItemCount(): Int {
     return exercises.size
   }
 
-  fun showExercises(newList: List<ExerciseModel> ) {
+  fun showExercises(newList: List<ExerciseModel>) {
     this.exercises = newList
     notifyDataSetChanged()
   }
