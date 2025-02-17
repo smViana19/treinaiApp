55a759e43251e7f44c8f21b6523e779d23f61871
Samuu <samuelfilipevianadois@gmail.com>
2025-02-17 16:59:41 -0300

    feat: add coroutines test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
   implementation(libs.androidx.lifecycle.livedata.ktx)
   implementation(libs.androidx.lifecycle.viewmodel.ktx)
   testImplementation(libs.junit)
+  testImplementation (libs.kotlinx.coroutines.test)
+  testImplementation(libs.androidx.core.testing)
   testImplementation (libs.mockk.android)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
